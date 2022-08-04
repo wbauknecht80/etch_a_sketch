484d5114f4e45b5fc481f264239d2c020a3bfabb
wbauknecht80 <wbauknecht80@gmail.com>
2022-08-04 12:20:58 -0500

    Create grid & implement color change on hover Need to create input for user to select gird Need to implement shading Implement randomized color???

--- javascript.js
+++ javascript.js
@@ -1,14 +1,23 @@
-var numBoxes = 16
+const container = document.getElementById("container");
 
-var arrayDiv = new Array()
-for (i=0; i < numBoxes; i++) {
-    arrayDiv[i] = document.createElement('div');
-    arrayDiv[i].id = 'box' + i;
-    arrayDiv[i].style.backgroundColor = 'green';
-    arrayDiv[i].className = 'box' + i;
-    arrayDiv[i].textContent = i
-    document.body.appendChild(arrayDiv[i])
+
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grind-cols', cols);
+    for (c= 0; c <(rows*cols); c++) {
+        let cell = document.createElement("div")
+        cell.innerText = (c+1);
+        container.appendChild(cell).className = "grid-item";
+        
+    }
 }
 
-arrayDiv.stlye.display = 'flex'
-arrayDiv.style.flexdirection = 'column'
+/**function randomColor() {
+    r = Math.random() * 255;
+    g = Math.random() * 255;
+    b = Math.random() * 225;
+}**/
+
+makeRows(16, 16);
+
